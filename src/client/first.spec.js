d12ee7f26af4b9e8b1d8c49ddd5bc4501ec836be
Artem Drobotenko <artem.drobotenko@gmail.com>
2017-11-16 01:55:31 +0200

    reformat unit tests in groups

--- src/client/first.spec.js
+++ src/client/first.spec.js
@@ -1,48 +1,60 @@
-describe('first suite', function(){
-  it('works', function(){
+describe('sanity test', function () {
+  it('passes', function () {
     expect(true).toBe(true);
   });
 });
 
-describe('first suite', function(){
-  it('has Object.create', function() {
-    expect(Object.create).toEqual(jasmine.any(Function));
-  });
-  it('has Object.assign', () => {
-    expect(Object.assign).toEqual(jasmine.any(Function));
-  });
-  it('has Object.assign', function(){
-    expect(Promise).toEqual(jasmine.any(Function));
-  });
-  it('can recognize arrow functions', function() {
-    expect(() => 123).toEqual(jasmine.any(Function))
-  });
-  it('should not throw', function(done){
-    window.setTimeout = function(cb) {
-      cb();
-    };
-    var val = null;
-    window.setTimeout(function() {
-      val = 123;
-      done();
-    }, 10500);
-    expect(val).toBe(123);
+describe('first suite', function () {
+  describe('example of an async->sync test double', function () {
+    it('runs in a sync manner', function (/*done*/) {
+      window.setTimeout = function (cb) {
+        cb();
+      };
+      var val = null;
+      window.setTimeout(function () {
+        val = 123;
+        // done();
+      }, 10500);
+      expect(val).toBe(123);
+    });
   });
 
-  // it('can fetch', function(){
-  //   expect(fetch('google.com')).toEqual(jasmine.any(Promise));
+  describe('ES5 features', function () {
+    it('has Object.create', function () {
+      expect(Object.create).toEqual(jasmine.any(Function));
+    });
+  });
+
+  describe('ES2015 features', function () {
+    it('has Promise', function () {
+      expect(Promise).toEqual(jasmine.any(Function));
+    });
+
+    it('has Object.assign', function () {
+      expect(Object.assign).toEqual(jasmine.any(Function));
+    });
+
+    it('can recognize arrow functions', function () {
+      expect(() => 123).toEqual(jasmine.any(Function))
+    });
+
+    it('can use desctructuring assignment', function () {
+      let a = 1;
+      let b = 2;
+
+      // flip them
+      [a, b] = [b, a];
+
+      expect(a).toEqual(2);
+      expect(b).toEqual(1);
+    });
+  });
+
+  // describe('ES2017 features', function () {
+  //   it('can fetch', function(){
+  //     expect(fetch('google.com')).toEqual(jasmine.any(Promise));
+  //   });
   // });
-
-  it('can use desctructuring assignment', function(){
-    let a = 1;
-    let b = 2;
-
-    // flip them
-    [a, b] = [b, a];
-
-    expect(a).toEqual(2);
-    expect(b).toEqual(1);
-  });
 
 
 });
